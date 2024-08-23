f9bf65d53614ec19bce593b89c6a468f97d4e0c0
Kostandinos <topapi30@gmail.com>
2024-08-23 20:05:04 +0300

    initial stage

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/guitar01.png' 
+import imageProduct2 from './assets/images/guitar02.png' 
+import imageProduct3 from './assets/images/guitar03.png' 
+import imageProduct4 from './assets/images/guitar04.png' 
+import imageProduct5 from './assets/images/guitar05.png' 
+import imageProduct6 from './assets/images/guitar06.png' 
+import imageProduct7 from './assets/images/guitar07.png' 
+import imageProduct8 from './assets/images/guitar08.png' 
+export const products = [
+    {
+        id: 1,
+        name: 'harley benton 559',
+        price: 101,
+        image: imageProduct1,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'fender Stratocaster brown',
+        price: 151,
+        image: imageProduct2,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Gibson Les Paul black',
+        price: 181,
+        image: imageProduct3,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Jertel 29jh',
+        price: 201,
+        image: imageProduct4,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Gibson fly V',
+        price: 251,
+        image: imageProduct5,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Japanese quantum 69',
+        price: 111,
+        image: imageProduct6,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Hollow serial 98',
+        price: 121,
+        image: imageProduct7,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Stephens Moore little bitch',
+        price: 220,
+        image: imageProduct8,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+];
